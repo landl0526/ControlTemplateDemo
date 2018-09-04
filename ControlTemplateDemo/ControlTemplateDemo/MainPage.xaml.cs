e6c2b3316b6710866133942a45ecb362b615efb3
Land Lu <v-luting@microsoft.com>
2018-09-04 10:04:27 +0800

    Add project files.

---
+++ ControlTemplateDemo/ControlTemplateDemo/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ControlTemplateDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public static readonly BindableProperty TapCommandProperty = BindableProperty.Create("TapCommand", typeof(Command), typeof(MainPage), null);
+        public Command TapCommand
+        {
+            set
+            {
+                SetValue(TapCommandProperty, value);
+            }
+            get
+            {
+                return (Command)GetValue(TapCommandProperty);
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ControlTemplateDemo/ControlTemplateDemo/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ControlTemplateDemo
+{
+    public class MainPageViewModel
+    {
+        public Command TapCommand { set; get; }
+
+        public MainPageViewModel()
+        {
+            TapCommand = new Command(() =>
+            {
+                // Do something
+            });
+        }
+    }
+}
